4e2984d65ac0f805a4e5d9ec5b2ae252d831e2ac
zhangdong <zhangdonginpeking@gmail.com>
2015-12-16 00:07:30 +0800

    modify hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,6 @@
 public class HelloWorld {
+        private String name;
+        private int age;
 	public static void main(String[] args){
 		System.out.println("HelloWorld");
 		System.out.println("HelloWorld");
